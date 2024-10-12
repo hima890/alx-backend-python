62fbb1b8208174632bcb8b5feef25300e0599335
hima890 <hfibrahim90@gmail.com>
2024-10-12 07:33:04 +0200

    add/task-1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+A type-annotated function concat that takes a string str1 and a string str2 as
+arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function Name: concat
+
+    Description:
+        Function concat that takes a string str1 and a string str2 as
+        arguments and returns a concatenated string
+
+    Args:
+        str1 (str): str input
+        str2 (str): str input
+
+    Returns:
+        str: A concatenated string
+    """
+    return(str1 + str2)
